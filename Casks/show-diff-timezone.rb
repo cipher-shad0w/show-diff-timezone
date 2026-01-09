003ca7799c36c4c54f672c42bf7daaec9b901c6e
Cipher Shad0w <jannis.krija@icloud.com>
2026-01-09 13:10:17 +0100

    🔖 Bump version to 0.1.6

--- Casks/show-diff-timezone.rb
+++ Casks/show-diff-timezone.rb
@@ -1,5 +1,5 @@
 cask "show-diff-timezone" do
-  version "0.1.3"
+  version "0.1.6"
   sha256 "d5d28aab9435cf8a73f709e2bad9853424ed645b65874b0e2a0584c56cb3745a"
 
   url "https://github.com/cipher-shad0w/show-diff-timezone/releases/download/v#{version}/show-diff-timezone.zip"
